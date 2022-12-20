d94b25428297af268fefc43a0c0f761514d3d19a
Hinoki Crum <hinokicrum@gmail.com>
2022-12-19 16:53:07 -0800

    python file

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Financial-Sentiment-Analysis
+
+This study aims to perform sentiment analysis on the financial textual data available at Kaggle (https://www.kaggle.com/datasets/sbhatti/financial-sentiment-analysis) and to construct classification models by the following machine learning algorithms - Logistic Regression, Support Vector Machine, Decision Tree, Random Forest and  Multinomial Naive Bayes. To apply those machine learning models, I converted the textual data into a numerical representation using Bag of Words and TF-IDF, and compared the performance. To deal with the imbalanced classifications, I used various over-sampling techniques along with the cost-sensitive learning.  
+
+Support Vector Machine worked the best followed by Logistic Regression. RandomOverSampler was the best technique rather than SMOTE or combination of SMOTE and RandomUnderSampler to deal with my imbalance data.  TF-IDF generally worked better than Bag of Words as Bag of Words worked well only when randomly over-sampled.  
+
+Overall, the models performed better when the text was not cleaned by removing the stop words and punctuation. It appears to work better not to remove the stop words to preserve the entire meaning of the context as when performing the sentiment analysis, it could cause a misclassification of the sentiment. 
+
+Support Vector Machine with text as is (stopwords and punctuation not removed), TF-IDF and RandomOverSampler when  resulted in the best F1 score of 0.85.
+
+RandomOverSampler significantly improved the model performance on the imbalanced data, but the predictions were still biased towards the majority class. 
---
+++ finacial_sentiment.py
@@ -0,0 +1,150 @@
+import pandas as pd
+import nltk
+import string
+import csv
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+#conda install -c glemaitre imbalanced-learn
+from collections import Counter
+from imblearn.over_sampling import SMOTE
+from imblearn.over_sampling import RandomOverSampler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from imblearn.under_sampling import RandomUnderSampler
+from sklearn.metrics import f1_score, accuracy_score
+import numpy as np
+np.random.seed(111)
+stopwords = nltk.corpus.stopwords.words('english')
+import warnings
+warnings.filterwarnings("ignore")
+
+class TextProcessing:
+    def __init__(self, file):
+        # read the data using pandas
+        self.df = pd.read_csv(file)
+        self.sent = self.df['Sentence'].astype(str).str.lower()  # text not cleaned
+        self.no_punc_sent = self._remove_punc()
+        self.cleaned_sent = self._process_text()  # Text cleaned
+        self.labels = self._process_labels()
+
+    # remove punctuation only
+    def _remove_punc(self):
+        text = self.sent.apply(lambda x: ' '.join(
+            [word for word in x.split() if word not in string.punctuation]))
+        return text
+
+    # process data by lowering, removing stopwords & punctuation
+    def _process_text(self):
+        sentences = self.sent.apply(lambda x: ' '.join([word for word \
+            in x.split() if word not in set(stopwords) and string.punctuation]))
+        return sentences
+
+    def tfidf_vectorizer(self, text):  #tf-idf vectorizer
+        vectorizer = TfidfVectorizer()
+        tf_idf = vectorizer.fit_transform(text).toarray()
+        return tf_idf
+
+    def bag_of_words(self, text):  # bag_of_word vectorizer
+        vectorizer = CountVectorizer()
+        bow = vectorizer.fit_transform(text).toarray()
+        return bow
+
+    def _process_labels(self):
+        labels = self.df['Sentiment'].apply(lambda x: -1 if \
+            x == 'negative' else (1 if x == 'positive' else 0))
+        return labels
+
+    # to oversample minority class using SMOTE
+    def oversample_smote(self, text, sampling_strategy='auto'):
+        oversample = SMOTE(sampling_strategy=sampling_strategy)
+        oversample_sent, labels_final = oversample.fit_resample(text, self.labels)
+        counter = Counter(labels_final)
+        print(counter)
+        return oversample_sent, labels_final
+
+    # to randomly oversample by RandomOverSampler
+    def oversample_random(self, text):
+        oversample = RandomOverSampler()
+        oversample_sent, labels_final = oversample.fit_resample(text, self.labels)
+        return oversample_sent, labels_final
+
+    # to randomly undersample by RandomUnderSampler
+    def under_over_sample(self, text, sampling_strategy):
+        x, y = self.oversample_smote(text)
+        oversample = RandomUnderSampler(sampling_strategy=sampling_strategy)
+        oversample_sent, labels_final = oversample.fit_resample(x,y)
+        counter = Counter(labels_final)
+        print(counter)
+        return oversample_sent, labels_final
+
+
+# define models with cost sensitive approach and specific hyperparameters
+MODELS = {'Logistic Regression': LogisticRegression(multi_class='multinomial',
+                      class_weight='balanced', C=10, penalty='l2'),
+          'SVM': SVC(class_weight='balanced', C=10),
+          'Random Forest': RandomForestClassifier(class_weight='balanced')}
+
+# to select best model based on weighted average F1 score on validation set
+def best_model(models, text, labels, measure):
+    train_x, val_x, train_y, val_y = train_test_split(text, labels, test_size=0.3)
+
+    for name, model in models.items():
+        model.fit(train_x, train_y)
+        pred = model.predict(val_x)
+        if measure == 'f1':
+            print(name, f1_score(val_y, pred, average='weighted'))
+        elif measure == 'accuracy':
+            print(name, accuracy_score(val_y, pred))
+        else:
+            print('Please select f1, f1_weighted or accuracy as metric.')
+
+
+text_proc = TextProcessing('data.csv')
+
+sampling_strategy = {-1: 2800, 0: 3130, 1: 2800}
+
+tfidf = text_proc.oversample_random(text_proc.tfidf_vectorizer(text_proc.sent))
+train_x, val_x, train_y, val_y = train_test_split(tfidf[0], tfidf[1], test_size=0.3)
+
+bow = text_proc.oversample_random(text_proc.bag_of_words(text_proc.no_punc_sent))
+train_xx, val_xx, train_yy, val_yy = train_test_split(bow[0], bow[1], test_size=0.3)
+
+model = MODELS['SVM']
+
+model.fit(train_x, train_y)
+tfidf_pred = model.predict(val_x)
+print("TFIDF As Is - Random")
+print(f1_score(val_y, tfidf_pred, average='weighted'))
+
+model.fit(train_xx, train_yy)
+bow_pred = model.predict(val_xx)
+print("BoW No Punctuation - Random")
+print(f1_score(val_yy, bow_pred, average='weighted'))
+
+'''
+print("BOW Cleaned - Random")
+over_samp = text_proc.oversample_random(text_proc.bag_of_words(text_proc.cleaned_sent))
+best_model(MODELS, over_samp[0], over_samp[1], 'f1')
+
+print("TFIDF Cleaned - Random")
+comb = text_proc.oversample_random(text_proc.tfidf_vectorizer(text_proc.cleaned_sent))
+best_model(MODELS, comb[0], comb[1], 'f1')
+
+print("TFIDF Cleaned - SMOTE")
+comb = text_proc.oversample_smote(text_proc.tfidf_vectorizer(text_proc.cleaned_sent))
+best_model(MODELS, comb[0], comb[1], 'f1')
+
+print("TFIDF Cleaned - Combination")
+comb = text_proc.under_over_sample(text_proc.tfidf_vectorizer(text_proc.cleaned_sent), sampling_strategy)
+best_model(MODELS, comb[0], comb[1], 'f1')
+'''
+
+
+
+
+
+
+
+
+
